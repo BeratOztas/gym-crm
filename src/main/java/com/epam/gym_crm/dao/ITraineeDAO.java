3e8fcc35aad52f641b5066df090f9cdcb1be2813
Berat <berat230@gmail.com>
2025-07-13 22:55:15 +0300

    feat(dao): Add findByUsername method to ITraineeDAO

--- src/main/java/com/epam/gym_crm/dao/ITraineeDAO.java
+++ src/main/java/com/epam/gym_crm/dao/ITraineeDAO.java
@@ -1,7 +1,9 @@
 package com.epam.gym_crm.dao;
 
+import java.util.Optional;
+
 import com.epam.gym_crm.model.Trainee;
 
-public interface ITraineeDAO  extends BaseDAO<Trainee>{
-
+public interface ITraineeDAO extends BaseDAO<Trainee> {
+	Optional<Trainee> findByUsername(String username);
 }
