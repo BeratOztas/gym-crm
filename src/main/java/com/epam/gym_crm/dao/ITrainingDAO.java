a28cc6619f615e36769e71c9c22a9a32b8e373b5
Berat <berat230@gmail.com>
2025-07-14 01:13:05 +0300

    feat(dao): Enhance ITrainingDAO with specific search methods
    The added methods enable flexible data retrieval:
    - `findByTrainingName(String trainingName)`: To find trainings by their name.
    - `findByTrainingType(TrainingType trainingType)`: To find trainings by their category (e.g., YOGA, FITNESS).
    - `findByTrainingDate(LocalDate trainingDate)`: To find trainings scheduled on a specific date.
    - `findByTraineeId(Long traineeId)`: To retrieve all trainings associated with a particular trainee.
    - `findByTrainerId(Long trainerId)`: To retrieve all trainings conducted by a specific trainer.

--- src/main/java/com/epam/gym_crm/dao/ITrainingDAO.java
+++ src/main/java/com/epam/gym_crm/dao/ITrainingDAO.java
@@ -1,7 +1,20 @@
 package com.epam.gym_crm.dao;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import com.epam.gym_crm.model.Training;
+import com.epam.gym_crm.model.TrainingType;
 
-public interface ITrainingDAO  extends BaseDAO<Training>{
+public interface ITrainingDAO extends BaseDAO<Training> {
 
+	List<Training> findByTrainingName(String trainingName);
+
+	List<Training> findByTrainingType(TrainingType trainingType);
+
+	List<Training> findByTrainingDate(LocalDate trainingDate);
+
+	List<Training> findByTraineeId(Long traineeId);
+
+	List<Training> findByTrainerId(Long trainerId);
 }
