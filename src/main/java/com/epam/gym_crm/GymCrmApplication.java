b8a2a602acda635c130de568cd638b4cbe28db62
Berat <berat230@gmail.com>
2025-07-14 05:21:24 +0300

    refactor: Streamline Main Application Entry Point

--- src/main/java/com/epam/gym_crm/GymCrmApplication.java
+++ src/main/java/com/epam/gym_crm/GymCrmApplication.java
@@ -1,13 +1,24 @@
 package com.epam.gym_crm;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.epam.gym_crm.facade.GymCRMFacade;
 
 @SpringBootApplication
 public class GymCrmApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(GymCrmApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(GymCrmApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(GymCrmApplication.class, args);
+		GymCRMFacade facade = context.getBean(GymCRMFacade.class);
+
+		logger.info("--- Gym CRM Application Started Successfully ---");
+		 
 	}
 
 }
