5e242111fa1a2f973cdbd491a13d45b80ae8eebc
Berat <berat230@gmail.com>
2025-07-14 05:12:46 +0300

    feat:Add ITrainingService interface with CRUD and search methods

---
+++ src/main/java/com/epam/gym_crm/service/ITrainingService.java
@@ -0,0 +1,30 @@
+package com.epam.gym_crm.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.epam.gym_crm.model.Training;
+import com.epam.gym_crm.model.TrainingType;
+
+public interface ITrainingService {
+
+	Training findById(Long id);
+
+    List<Training> getAllTrainings();
+
+    Training create(Training training);
+
+    Training update(Training training);
+
+    boolean delete(Long id);
+    
+	List<Training> findByTrainingName(String trainingName);
+
+	List<Training> findByTrainingType(TrainingType trainingType);
+
+	List<Training> findByTrainingDate(LocalDate trainingDate);
+
+	List<Training> findByTraineeId(Long traineeId);
+
+	List<Training> findByTrainerId(Long trainerId);
+}
