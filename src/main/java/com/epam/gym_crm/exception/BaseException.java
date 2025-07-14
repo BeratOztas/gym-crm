233fedfbe7f8a330edfe2a7a9844ecd51848af32
Berat <berat230@gmail.com>
2025-07-15 00:53:21 +0300

    feat: Add serialVersionUID to BaseException for serialization

--- src/main/java/com/epam/gym_crm/exception/BaseException.java
+++ src/main/java/com/epam/gym_crm/exception/BaseException.java
@@ -1,7 +1,9 @@
 package com.epam.gym_crm.exception;
 
 public class BaseException extends RuntimeException {
-	
+
+	private static final long serialVersionUID = 5674758076774251773L;
+
 	public BaseException(ErrorMessage errorMessage) {
 		super(errorMessage.prepareErrorMessage());
 	}
