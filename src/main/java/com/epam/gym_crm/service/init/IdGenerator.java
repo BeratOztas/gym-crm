97c7026eedcf4dec6807b7b73cde0bb208ffb9b0
Berat <berat230@gmail.com>
2025-07-14 05:15:59 +0300

    refactor: IdGenrator add BaseException with meaningful error messages

--- src/main/java/com/epam/gym_crm/service/init/IdGenerator.java
+++ src/main/java/com/epam/gym_crm/service/init/IdGenerator.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.epam.gym_crm.exception.BaseException;
+import com.epam.gym_crm.exception.ErrorMessage;
+import com.epam.gym_crm.exception.MessageType;
 import com.epam.gym_crm.utils.EntityType;
 
 import java.util.Map;
@@ -28,7 +31,8 @@ public class IdGenerator {
 		AtomicLong counter = idCounters.get(entityType);
 		if (counter == null) {
 			logger.error("ID counter not found for entity type '{}'. Please ensure it's initialized.", entityType);
-			throw new IllegalArgumentException("ID counter not found for entity type: " + entityType);
+			throw new BaseException(new ErrorMessage(MessageType.INVALID_ARGUMENT, 
+                    "ID counter not found for entity type: " + entityType + ". Please check configuration."));
 		}
 		return counter.incrementAndGet();
 	}
