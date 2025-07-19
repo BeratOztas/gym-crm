d8a67ab4389e59f086daf2318327921c88fb0dd6
Berat <berat.oztas.dev@gmail.com>
2025-07-19 11:43:37 +0300

    feat: Configure JPA/Hibernate for PostgreSQL database connection
    Modified AppConfig.java
    Added DataSource, LocalContainerEntityManagerFactoryBean, and PlatformTransactionManager beans for JPA/Hibernate setup.
        - Integrated property values from application.properties using @Value.

--- src/main/java/com/epam/gym_crm/GymCrmApplication.java
+++ src/main/java/com/epam/gym_crm/GymCrmApplication.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import com.epam.gym_crm.config.AppConfig;
 
+import jakarta.persistence.EntityManagerFactory;
+
 public class GymCrmApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(GymCrmApplication.class);
@@ -14,6 +16,12 @@ public class GymCrmApplication {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		try {
 			logger.info("--- Gym CRM Application Started Successfully ---");
+			
+			EntityManagerFactory emf = context.getBean(EntityManagerFactory.class);
+
+	        System.out.println("✅ Bağlantı başarılı: " + emf.isOpen());
+
+	        emf.close();
 		} finally {
 			context.close();
 		}
--- src/main/java/com/epam/gym_crm/config/AppConfig.java
+++ src/main/java/com/epam/gym_crm/config/AppConfig.java
@@ -1,14 +1,23 @@
 package com.epam.gym_crm.config;
 
 import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
-
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import com.epam.gym_crm.model.Trainee;
 import com.epam.gym_crm.model.Trainer;
 import com.epam.gym_crm.model.Training;
@@ -16,11 +25,70 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+import jakarta.persistence.EntityManagerFactory;
+
 @Configuration
 @ComponentScan(basePackages = "com.epam.gym_crm")
 @PropertySource("classpath:application.properties")
+@EnableJpaRepositories(basePackages = "com.epam.gym_crm")
 @EnableAspectJAutoProxy
 public class AppConfig {
+	
+	@Value("${db.driver}")
+    private String driver;
+
+    @Value("${db.url}")
+    private String url;
+
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
+    @Value("${hibernate.dialect}")
+    private String dialect;
+
+    @Value("${hibernate.show_sql}")
+    private String showSql;
+
+    @Value("${hibernate.format_sql}")
+    private String formatSql;
+
+    @Value("${hibernate.hbm2ddl.auto}")
+    private String hbm2ddlAuto;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName(driver);
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        return ds;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource());
+        emf.setPackagesToScan("com.epam.gym_crm.model");
+        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties props = new Properties();
+        props.setProperty("hibernate.dialect", dialect);
+        props.setProperty("hibernate.show_sql", showSql);
+        props.setProperty("hibernate.format_sql", formatSql);
+        props.setProperty("hibernate.hbm2ddl.auto", hbm2ddlAuto);
+
+        emf.setJpaProperties(props);
+        return emf;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
 	
 	@Bean(name = "traineeStorageMap")
 	public Map<Long, Trainee> traineeStorageMap(){
