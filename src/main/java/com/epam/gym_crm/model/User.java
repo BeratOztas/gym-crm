cf1091fad61129fe3b8d41cd7b7979d28a48b28a
Berat <berat.oztas.dev@gmail.com>
2025-07-20 12:13:18 +0300

    feat: add User entity with basic fields and validation

--- src/main/java/com/epam/gym_crm/model/User.java
+++ src/main/java/com/epam/gym_crm/model/User.java
@@ -1,106 +1,56 @@
 package com.epam.gym_crm.model;
 
-import java.util.Objects;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@NotBlank(message = "First name cannot be blank") 
 	@Size(min = 3, max = 50, message = "First name must be between 2 and 50 characters") 
+	@Column(name = "first_name",nullable = false)
 	private String firstName;
 	@NotBlank(message = "Last name cannot be blank") 
 	@Size(min = 3, max = 50, message = "Last name must be between 2 and 50 characters") 
+	@Column(name = "last_name",nullable =  false)
 	private String lastName;
 	
+	@NotBlank(message = "Username cannot be blank")
+	@Column(name = "username",nullable = false,unique = true)
 	private String username;
+	
+	@Column(name = "password",nullable = false)
 	private String password;
+	
 	@NotNull(message = "Active status cannot be null")
+	@Column(name = "is_active", nullable = false)
 	private boolean isActive;
+	
+	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private Trainee trainee;
 
-	public User() {
-	}
-
-	public User(Long id, String firstName, String lastName, String username, String password, boolean isActive) {
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.username = username;
-		this.password = password;
-		this.isActive = isActive;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public boolean isActive() {
-		return isActive;
-	}
-
-	public void setActive(boolean isActive) {
-		this.isActive = isActive;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null || getClass() != obj.getClass())
-			return false;
-		User user = (User) obj;
-		return Objects.equals(id, user.id);
-	}
-
-	@Override
-	public String toString() {
-		return "User{" + "id=" + id + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\''
-				+ ", username='" + username + '\'' + ", isActive=" + isActive + '}';
-	}
+	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private Trainer trainer;
+	
 
 }
