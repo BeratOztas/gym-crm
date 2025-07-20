96c6183b520e40055cdc7b7b2df1223981742a0e
Berat <berat.oztas.dev@gmail.com>
2025-07-20 12:13:51 +0300

    feat: add TrainingType entity for defining training categories

--- src/main/java/com/epam/gym_crm/model/TrainingType.java
+++ src/main/java/com/epam/gym_crm/model/TrainingType.java
@@ -1,9 +1,29 @@
 package com.epam.gym_crm.model;
 
-public enum TrainingType {
-	FITNESS,
-    YOGA,
-    ZUMBA,
-    STRETCHING,
-    RESISTANCE;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "training_type")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class TrainingType {
+
+	@Id
+	private Long id;
+
+	@NotBlank(message = "Training type name cannot be blank")
+	@Size(min = 3, max = 50, message = "Training type name must be between 3 and 50 characters")
+	@Column(name = "training_type_name", nullable = false, unique = true)
+	private String trainingTypeName;
+	
+
 }
