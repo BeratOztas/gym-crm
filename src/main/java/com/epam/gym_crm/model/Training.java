0368a971702c45807c353d6e93b0f807c15116dd
Berat <berat.oztas.dev@gmail.com>
2025-07-20 12:14:01 +0300

    feat: add Training entity and define relationships with Trainee, Trainer, and TrainingType

--- src/main/java/com/epam/gym_crm/model/Training.java
+++ src/main/java/com/epam/gym_crm/model/Training.java
@@ -1,128 +1,60 @@
 package com.epam.gym_crm.model;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Entity
+@Table(name = "training")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Training {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	@NotBlank(message = "Training name cannot be blank") 
 	@Size(min = 3, max = 100, message = "Training name must be between 3 and 100 characters") 
+	@Column(name = "training_name",nullable = false)
 	private String trainingName;
-	@NotNull(message = "Training type cannot be null")
-	private TrainingType trainingType;
+	
 	@NotNull(message = "Training date cannot be null") 
+	@Column(name = "training_date",nullable = false)
 	private LocalDate trainingDate;
+	
 	@NotNull(message = "Training duration cannot be null") 
     @Min(value = 1, message = "Training duration must be at least 1 minute")
+	@Column(name ="training_duration",nullable = false)
 	private Integer trainingDuration;
 
+	@ManyToOne(optional = false)
+    @JoinColumn(name = "trainee_id", nullable = false)
 	@NotNull(message = "Trainee cannot be null for a training")
-	private Trainee trainee;
-	@NotNull(message = "Trainer cannot be null for a training")
-	private Trainer trainer;
+    private Trainee trainee;
 
-	public Training() {
-	}
-
-	public Training(Long id, String trainingName, TrainingType trainingType, LocalDate trainingDate, Integer duration,
-			Trainee trainee, Trainer trainer) {
-		this.id = id;
-		this.trainingName = trainingName;
-		this.trainingType = trainingType;
-		this.trainingDate = trainingDate;
-		this.trainingDuration = duration;
-		this.trainee = trainee;
-		this.trainer = trainer;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getTrainingName() {
-		return trainingName;
-	}
-
-	public void setTrainingName(String trainingName) {
-		this.trainingName = trainingName;
-	}
-
-	public TrainingType getTrainingType() {
-		return trainingType;
-	}
-
-	public void setTrainingType(TrainingType trainingType) {
-		this.trainingType = trainingType;
-	}
-
-	public LocalDate getTrainingDate() {
-		return trainingDate;
-	}
-
-	public void setTrainingDate(LocalDate trainingDate) {
-		this.trainingDate = trainingDate;
-	}
-
-	public Integer getTrainingDuration() {
-		return trainingDuration;
-	}
-
-	public void setTrainingDuration(Integer trainingDuration) {
-		this.trainingDuration = trainingDuration;
-	}
-
-	public Trainee getTrainee() {
-		return trainee;
-	}
-
-	public void setTrainee(Trainee trainee) {
-		this.trainee = trainee;
-	}
-
-	public Trainer getTrainer() {
-		return trainer;
-	}
-
-	public void setTrainer(Trainer trainer) {
-		this.trainer = trainer;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null || getClass() != obj.getClass())
-			return false;
-		Training training = (Training) obj;
-		return Objects.equals(id, training.id);
-	}
-
-	@Override
-	public String toString() {
-		 return "Training{" +
-	               "id=" + id +
-	               ", traineeId=" + (trainee != null && trainee.getUser() != null ? trainee.getUser().getId() : "null") +
-	               ", trainerId=" + (trainer != null && trainer.getUser() != null ? trainer.getUser().getId() : "null") +
-	               ", trainingName='" + trainingName + '\'' +
-	               ", trainingType=" + (trainingType != null ? trainingType.name() : "null") + 
-	               ", trainingDate=" + trainingDate +
-	               ", trainingDuration=" + trainingDuration +
-	               '}';
-	}
-
+    @ManyToOne(optional = true)
+    @JoinColumn(name = "trainer_id", nullable = true)
+//    @NotNull(message = "Trainer cannot be null for a training")
+    private Trainer trainer;
+	
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "training_type_id",nullable = false)
+    private TrainingType trainingType;
 }
