d836c2dfec8d50ec1daadd7d3e474d79531d8981
Berat <berat.oztas.dev@gmail.com>
2025-07-20 12:13:41 +0300

    feat: add Trainer entity and establish OneToOne relation with User

--- src/main/java/com/epam/gym_crm/model/Trainer.java
+++ src/main/java/com/epam/gym_crm/model/Trainer.java
@@ -1,62 +1,44 @@
 package com.epam.gym_crm.model;
 
-import java.util.Objects;
+import java.util.HashSet;
+import java.util.Set;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "trainer")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Trainer {
-	@NotNull(message = "Specialization cannot be null")
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "training_type_id",nullable = false)
 	private TrainingType specialization;
-
+	
+	@OneToOne(optional = false)
+	@JoinColumn(name = "user_id",nullable = false,unique = true)
 	private User user;
+	
+	@ManyToMany(mappedBy = "trainers")
+    private Set<Trainee> trainees = new HashSet<>();
 
-	public Trainer() {
-		this.user = new User();
-	}
-
-	public Trainer(Long id, String firstName, String lastName, String username, String password, boolean isActive,
-			TrainingType specialization) {
-		this.user = new User(id, firstName, lastName, username, password, isActive);
-		this.specialization = specialization;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public TrainingType getSpecialization() {
-		return specialization;
-	}
-
-	public void setSpecialization(TrainingType specialization) {
-		this.specialization = specialization;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(super.hashCode(), specialization);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null || getClass() != obj.getClass())
-			return false;
-		if (!super.equals(obj))
-			return false;
-		Trainer trainer = (Trainer) obj;
-		return specialization == trainer.specialization;
-	}
-
-	@Override
-	public String toString() {
-		return "Trainer{" + super.toString() + ", specialization="
-				+ (specialization != null ? specialization.name() : "null") + '}';
-	}
-
+	@OneToMany(mappedBy = "trainer") 
+	private Set<Training> trainings = new HashSet<>();
 }
