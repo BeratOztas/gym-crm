e44f7b8e228cf9428228f54dd733e786d57bab96
Berat <berat.oztas.dev@gmail.com>
2025-07-22 15:14:18 +0300

    Enhance MessageType with General Error Messages
    Added new MessageType entries: UNAUTHORIZED, ENTITY_NOT_FOUND, FORBIDDEN, and INVALID_STATE.

--- src/main/java/com/epam/gym_crm/exception/MessageType.java
+++ src/main/java/com/epam/gym_crm/exception/MessageType.java
@@ -4,6 +4,10 @@ public enum MessageType {
 
 	GENERAL_EXCEPTION("9999", "An unexpected error occurred."),
 	RESOURCE_NOT_FOUND("1001", "Resource not found."),
+	UNAUTHORIZED("401","User authentication required"),
+	ENTITY_NOT_FOUND("1010","Entity Not Found."),
+	FORBIDDEN("403","You are not authorized."),
+	INVALID_STATE("1011","User is not active"),
 	VALIDATION_ERROR("1003", "Validation failed."),
 	INVALID_ARGUMENT("1002", "Invalid parameter provided."),
 	DUPLICATE_USERNAME("1004", "Username already exists."),
