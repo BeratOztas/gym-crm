c6c68656a4effe28b170d0d3390dd383bb16d8f4
Berat <berat.oztas.dev@gmail.com>
2025-07-22 14:33:22 +0300

    feat: Remove redundant DAO layer, leverage Spring Data JPA

--- src/main/java/com/epam/gym_crm/dao/ITraineeDAO.java
+++
@@ -1,9 +0,0 @@
-package com.epam.gym_crm.dao;
-
-import java.util.Optional;
-
-import com.epam.gym_crm.model.Trainee;
-
-public interface ITraineeDAO extends BaseDAO<Trainee> {
-	Optional<Trainee> findByUsername(String username);
-}
--- src/main/java/com/epam/gym_crm/dao/ITrainerDAO.java
+++
@@ -1,9 +0,0 @@
-package com.epam.gym_crm.dao;
-
-import java.util.Optional;
-
-import com.epam.gym_crm.model.Trainer;
-
-public interface ITrainerDAO extends BaseDAO<Trainer> {
-	Optional<Trainer> findByUsername(String username);
-}
