c36d97173ddb8525390f783e54fbbaa808159c9d
Berat <berat.oztas.dev@gmail.com>
2025-07-22 14:50:15 +0300

    feat: Add findByUserUsername query method to TraineeRepository

--- src/main/java/com/epam/gym_crm/repository/TraineeRepository.java
+++ src/main/java/com/epam/gym_crm/repository/TraineeRepository.java
@@ -1,9 +1,13 @@
 package com.epam.gym_crm.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.epam.gym_crm.model.Trainee;
 
+@Repository
 public interface TraineeRepository extends JpaRepository<Trainee, Long> {
-
+	Optional<Trainee> findByUserUsername(String username);
 }
