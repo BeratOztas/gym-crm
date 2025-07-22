02d0e773b167f3052ae8f4d0f438959f46bf99cc
Berat <berat.oztas.dev@gmail.com>
2025-07-22 15:13:19 +0300

    Refactor ITrainingService to Align with Repository-Based Architecture and DTO Usage

--- src/main/java/com/epam/gym_crm/service/ITrainingService.java
+++ src/main/java/com/epam/gym_crm/service/ITrainingService.java
@@ -1,30 +1,27 @@
 package com.epam.gym_crm.service;
 
-import java.time.LocalDate;
 import java.util.List;
 
-import com.epam.gym_crm.model.Training;
+import com.epam.gym_crm.dto.request.TraineeTrainingListRequest;
+import com.epam.gym_crm.dto.request.TrainerTrainingListRequest;
+import com.epam.gym_crm.dto.request.TrainingCreateRequest;
+import com.epam.gym_crm.dto.request.TrainingUpdateRequest;
+import com.epam.gym_crm.dto.response.TrainingResponse;
 
 public interface ITrainingService {
 
-	Training findById(Long id);
+	TrainingResponse getTrainingById(Long id);
 
-    List<Training> getAllTrainings();
-
-    Training create(Training training);
-
-    Training update(Training training);
-
-    boolean delete(Long id);
+	List<TrainingResponse> getAllTrainings();
     
-	List<Training> findByTrainingName(String trainingName);
+    List<TrainingResponse> getTraineeTrainingsList(TraineeTrainingListRequest request);
 
-//	List<Training> findByTrainingType(TrainingType trainingType);
+    List<TrainingResponse> getTrainerTrainingsList(TrainerTrainingListRequest request);
+    
+    TrainingResponse createTraining(TrainingCreateRequest request);
 
-	List<Training> findByTrainingDate(LocalDate trainingDate);
-
-	List<Training> findByTraineeId(Long traineeId);
-
-	List<Training> findByTrainerId(Long trainerId);
+    TrainingResponse updateTraining(TrainingUpdateRequest request);
+	
+	void deleteTrainingById(Long id);
 }
 	
