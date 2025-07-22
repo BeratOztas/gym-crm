83dd0c55ec0b4a6e85bec2df4a795ce36b391188
Berat <berat.oztas.dev@gmail.com>
2025-07-22 14:56:56 +0300

    Enhance IAuthenticationService with Core Auth Operations
    include methods for user login, logout, password change, and a utility method for user creation.

---
+++ src/main/java/com/epam/gym_crm/service/IAuthenticationService.java
@@ -0,0 +1,16 @@
+package com.epam.gym_crm.service;
+
+import com.epam.gym_crm.dto.request.ChangePasswordRequest;
+import com.epam.gym_crm.dto.request.LoginRequest;
+import com.epam.gym_crm.model.User;
+
+public interface IAuthenticationService {
+
+	void login(LoginRequest request);
+
+	void logout();
+
+	User createAndSaveUser(String firstName, String lastName, boolean isActive);
+
+	void changePassword(ChangePasswordRequest request);
+}
