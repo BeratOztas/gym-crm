b1c589602ad5ea9a9d2f906575fcc8bc1fdf8152
Berat <berat.oztas.dev@gmail.com>
2025-07-22 15:08:00 +0300

    Refactor ITraineeService to Align with Repository-Based Architecture
    Implemented Methods:
    
    findTraineeById
    
    findTraineeByUsername
    
    getAllTrainees
    
    createTrainee
    
    updateTrainee
    
    updateTraineeTrainersList
    
    activateDeactivateTrainee
    
    deleteTraineeById
    
    deleteTraineeByUsername

--- src/main/java/com/epam/gym_crm/service/ITraineeService.java
+++ src/main/java/com/epam/gym_crm/service/ITraineeService.java
@@ -2,19 +2,29 @@ package com.epam.gym_crm.service;
 
 import java.util.List;
 
-import com.epam.gym_crm.model.Trainee;
+import com.epam.gym_crm.dto.request.TraineeCreateRequest;
+import com.epam.gym_crm.dto.request.TraineeUpdateRequest;
+import com.epam.gym_crm.dto.request.TraineeUpdateTrainersRequest;
+import com.epam.gym_crm.dto.request.UserActivationRequest;
+import com.epam.gym_crm.dto.response.TraineeResponse;
 
 public interface ITraineeService {
 
-	Trainee findTraineeById(Long id);
+	TraineeResponse findTraineeById(Long id);
 
-	Trainee findTraineeByUsername(String username);
+	TraineeResponse findTraineeByUsername(String username);
 
-	List<Trainee> getAllTrainees();
+	List<TraineeResponse> getAllTrainees();
 
-	Trainee create(Trainee trainee);
+	TraineeResponse createTrainee(TraineeCreateRequest request);
 
-	Trainee updateTrainee(Trainee trainee);
+	TraineeResponse updateTrainee(TraineeUpdateRequest trainee);
+	
+	TraineeResponse updateTraineeTrainersList(TraineeUpdateTrainersRequest request);
+	
+	void activateDeactivateTrainee(UserActivationRequest request);
 
-	boolean deleteTrainee(Long id);
+	void deleteTraineeById(Long id);
+	
+	void deleteTraineeByUsername(String username);
 }
