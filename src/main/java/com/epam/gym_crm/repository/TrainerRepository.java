8eb5507f6d4358586324f0a5600c5760ebecc103
Berat <berat.oztas.dev@gmail.com>
2025-07-22 14:51:15 +0300

    feat: Add custom query methods to TrainerRepository
    - Implemented `Optional<Trainer> findByUserUsername(String username);` for retrieving a Trainer by their associated User's username.
    - Added `List<Trainer> findActiveTrainersNotAssignedToTrainee(@Param("traineeUsername") String traineeUsername);` to fetch active trainers who are not currently assigned to a specific trainee.
    - Enhances Trainer lookup capabilities and supports the 'get unassigned trainers' use case.

--- src/main/java/com/epam/gym_crm/repository/TrainerRepository.java
+++ src/main/java/com/epam/gym_crm/repository/TrainerRepository.java
@@ -1,9 +1,27 @@
 package com.epam.gym_crm.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.epam.gym_crm.model.Trainer;
 
+@Repository
 public interface TrainerRepository extends JpaRepository<Trainer, Long> {
-
+	Optional<Trainer> findByUserUsername(String username);
+	
+	@Query("""
+	        SELECT tr FROM Trainer tr
+	        WHERE tr.user.isActive = true AND
+	        tr.id NOT IN (
+	            SELECT t.id FROM Trainee tra
+	            JOIN tra.trainers t
+	            WHERE tra.user.username = :traineeUsername
+	        )
+	    """)
+	    List<Trainer> findActiveTrainersNotAssignedToTrainee(@Param("traineeUsername") String traineeUsername);
 }
