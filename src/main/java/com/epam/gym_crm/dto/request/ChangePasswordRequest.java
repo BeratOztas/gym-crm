6b2940b4fcdf59eef2e8c4587efd3aaaf11f99f6
Berat <berat.oztas.dev@gmail.com>
2025-07-22 15:00:27 +0300

    Introduce LoginRequest and ChangePasswordRequest DTOs

---
+++ src/main/java/com/epam/gym_crm/dto/request/ChangePasswordRequest.java
@@ -0,0 +1,23 @@
+package com.epam.gym_crm.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChangePasswordRequest {
+
+	@NotBlank(message = "Username cannot be blank")
+	private String username;
+	
+	@NotBlank(message = "Old password cannot be blank")
+	private String oldPassword;
+	
+	@NotBlank(message = "New password cannot be blank")
+    @Size(min = 6, max = 100, message = "New password must be between 6 and 100 characters")
+	private String newPassword;
+}
---
+++ src/main/java/com/epam/gym_crm/dto/request/LoginRequest.java
@@ -0,0 +1,22 @@
+package com.epam.gym_crm.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+	@NotBlank(message = "Username cannot be blank")
+	@Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+	private String username;
+
+	@NotBlank(message = "Password cannot be blank")
+	@Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters") 
+	private String password;
+
+}
