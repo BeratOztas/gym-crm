2d452de9c30f0d82a7b2dfde2d0c5eed6433dc37
Berat <berat.oztas.dev@gmail.com>
2025-07-22 14:48:36 +0300

    Add @Repository Annotation to Repository Interfaces

--- src/main/java/com/epam/gym_crm/repository/TrainingTypeRepository.java
+++ src/main/java/com/epam/gym_crm/repository/TrainingTypeRepository.java
@@ -3,9 +3,11 @@ package com.epam.gym_crm.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.epam.gym_crm.model.TrainingType;
 
+@Repository
 public interface TrainingTypeRepository extends JpaRepository<TrainingType, Long> {
 	Optional<TrainingType> findByTrainingTypeNameIgnoreCase(String trainingTypeName);
 }
--- src/main/java/com/epam/gym_crm/repository/UserRepository.java
+++ src/main/java/com/epam/gym_crm/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.epam.gym_crm.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.epam.gym_crm.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	 Optional<User> findByUsername(String username);
 	 
