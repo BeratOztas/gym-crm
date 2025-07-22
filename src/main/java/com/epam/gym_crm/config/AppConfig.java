633ad0cedf94f320d7d521de3e00e2f217b619c6
Berat <berat.oztas.dev@gmail.com>
2025-07-22 14:53:26 +0300

    feat: Configure database connection and enable transaction management and remove storage maps
    - Updated AppConfig to include database connection beans (e.g., DataSource, EntityManagerFactory).
    - Added @EnableTransactionManagement annotation to enable Spring's declarative transaction management.
    - Ensures proper database connectivity and transactional integrity for service operations.

--- src/main/java/com/epam/gym_crm/config/AppConfig.java
+++ src/main/java/com/epam/gym_crm/config/AppConfig.java
@@ -1,8 +1,6 @@
 package com.epam.gym_crm.config;
 
-import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.ConcurrentHashMap;
 
 import javax.sql.DataSource;
 
@@ -18,9 +16,8 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
-import com.epam.gym_crm.model.Trainee;
-import com.epam.gym_crm.model.Trainer;
-import com.epam.gym_crm.model.Training;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -32,84 +29,70 @@ import jakarta.persistence.EntityManagerFactory;
 @PropertySource("classpath:application.properties")
 @EnableJpaRepositories(basePackages = "com.epam.gym_crm")
 @EnableAspectJAutoProxy
+@EnableTransactionManagement
 public class AppConfig {
-	
+
 	@Value("${db.driver}")
-    private String driver;
+	private String driver;
 
-    @Value("${db.url}")
-    private String url;
+	@Value("${db.url}")
+	private String url;
 
-    @Value("${db.username}")
-    private String username;
+	@Value("${db.username}")
+	private String username;
 
-    @Value("${db.password}")
-    private String password;
+	@Value("${db.password}")
+	private String password;
 
-    @Value("${hibernate.dialect}")
-    private String dialect;
+	@Value("${hibernate.dialect}")
+	private String dialect;
 
-    @Value("${hibernate.show_sql}")
-    private String showSql;
+	@Value("${hibernate.show_sql}")
+	private String showSql;
 
-    @Value("${hibernate.format_sql}")
-    private String formatSql;
+	@Value("${hibernate.format_sql}")
+	private String formatSql;
 
-    @Value("${hibernate.hbm2ddl.auto}")
-    private String hbm2ddlAuto;
+	@Value("${hibernate.hbm2ddl.auto}")
+	private String hbm2ddlAuto;
 
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource ds = new DriverManagerDataSource();
-        ds.setDriverClassName(driver);
-        ds.setUrl(url);
-        ds.setUsername(username);
-        ds.setPassword(password);
-        return ds;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-        emf.setDataSource(dataSource());
-        emf.setPackagesToScan("com.epam.gym_crm.model");
-        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-
-        Properties props = new Properties();
-        props.setProperty("hibernate.dialect", dialect);
-        props.setProperty("hibernate.show_sql", showSql);
-        props.setProperty("hibernate.format_sql", formatSql);
-        props.setProperty("hibernate.hbm2ddl.auto", hbm2ddlAuto);
-
-        emf.setJpaProperties(props);
-        return emf;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-        return new JpaTransactionManager(emf);
-    }
-	
-	@Bean(name = "traineeStorageMap")
-	public Map<Long, Trainee> traineeStorageMap(){
-		return new ConcurrentHashMap<>();
-	}
-
-	@Bean(name = "trainerStorageMap")
-	public Map<Long, Trainer> trainerStorageMap(){
-		return new ConcurrentHashMap<>();
-	}
-	
-	@Bean(name = "trainingStorageMap")
-	public Map<Long, Training> trainingStorageMap(){
-		return new ConcurrentHashMap<>();
-	}
-	
 	@Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule()); 
-        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        return mapper;
-    }
+	public DataSource dataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName(driver);
+		ds.setUrl(url);
+		ds.setUsername(username);
+		ds.setPassword(password);
+		return ds;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+		emf.setDataSource(dataSource());
+		emf.setPackagesToScan("com.epam.gym_crm.model");
+		emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+		Properties props = new Properties();
+		props.setProperty("hibernate.dialect", dialect);
+		props.setProperty("hibernate.show_sql", showSql);
+		props.setProperty("hibernate.format_sql", formatSql);
+		props.setProperty("hibernate.hbm2ddl.auto", hbm2ddlAuto);
+
+		emf.setJpaProperties(props);
+		return emf;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+		return new JpaTransactionManager(emf);
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new JavaTimeModule());
+		mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+		return mapper;
+	}
 }
