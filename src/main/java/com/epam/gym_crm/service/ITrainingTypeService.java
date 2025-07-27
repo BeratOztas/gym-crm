b4408c4bb434f7829cb28afd29af7159420dfad7
Berat <berat.oztas.dev@gmail.com>
2025-07-27 19:27:55 +0300

    feat: Implement TrainingTypeService for retrieving all training types.

---
+++ src/main/java/com/epam/gym_crm/service/ITrainingTypeService.java
@@ -0,0 +1,9 @@
+package com.epam.gym_crm.service;
+
+import java.util.List;
+
+import com.epam.gym_crm.model.TrainingType;
+
+public interface ITrainingTypeService {
+	List<TrainingType> getTrainingTypes();
+}
