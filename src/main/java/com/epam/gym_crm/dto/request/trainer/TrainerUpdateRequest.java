2fbf146f62f80146eb011b5e3d070646034b970d
Berat <berat.oztas.dev@gmail.com>
2025-07-27 19:15:26 +0300

    Update TrainerUpdateRequest DTO
    -added new fields to DTO

---
+++ src/main/java/com/epam/gym_crm/dto/request/trainer/TrainerUpdateRequest.java
@@ -0,0 +1,31 @@
+package com.epam.gym_crm.dto.request.trainer;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TrainerUpdateRequest {
+
+	@NotBlank(message = "Username is required")
+	private String username;
+
+	@NotBlank(message = "First name is required")
+	@Size(min = 3, max = 50, message = "First name must be between 3 and 50 characters")
+	private String firstName;
+
+	@NotBlank(message = "Last name is required")
+	@Size(min = 3, max = 50, message = "Last name must be between 3 and 50 characters")
+	private String lastName;
+
+	@NotBlank(message = "Specialization is required")
+	private String specialization; 
+
+	@NotNull(message = "Active status is required")
+	private boolean isActive;
+}
