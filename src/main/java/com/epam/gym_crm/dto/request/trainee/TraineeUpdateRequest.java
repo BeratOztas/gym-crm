3fbae34c8db74d95b7b63b087a2ce91fb96c680c
Berat <berat.oztas.dev@gmail.com>
2025-07-27 19:12:56 +0300

    Update:TraineeUpdateRequest
    -added logic valid anatations for fields
    -added isActive field

---
+++ src/main/java/com/epam/gym_crm/dto/request/trainee/TraineeUpdateRequest.java
@@ -0,0 +1,36 @@
+package com.epam.gym_crm.dto.request.trainee;
+
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TraineeUpdateRequest {
+
+	@NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "First name is required")
+    @Size(min = 3, max = 50, message = "First name must be between 3 and 50 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 3, max = 50, message = "Last name must be between 3 and 50 characters")
+    private String lastName;
+
+    @PastOrPresent(message = "Date of birth cannot be in the future")
+    private LocalDate dateOfBirth;
+
+    private String address;
+
+    @NotNull(message = "Active status is required") 
+    private boolean isActive;
+}
