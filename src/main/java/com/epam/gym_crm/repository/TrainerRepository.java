3368ee6e07550d7ff99200ca19b5db2ae48a6dc3
Berat <berat.oztas.dev@gmail.com>
2025-07-31 20:55:00 +0300

    feat:Add findByUserIsActive,remove:findActiveTrainersNotAssignedToTrainee refactored

--- src/main/java/com/epam/gym_crm/repository/TrainerRepository.java
+++ src/main/java/com/epam/gym_crm/repository/TrainerRepository.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.epam.gym_crm.model.Trainer;
@@ -13,15 +11,6 @@ import com.epam.gym_crm.model.Trainer;
 @Repository
 public interface TrainerRepository extends JpaRepository<Trainer, Long> {
 	Optional<Trainer> findByUserUsername(String username);
-	
-	@Query("""
-	        SELECT tr FROM Trainer tr
-	        WHERE tr.user.isActive = true AND
-	        tr.id NOT IN (
-	            SELECT t.id FROM Trainee tra
-	            JOIN tra.trainers t
-	            WHERE tra.user.username = :traineeUsername
-	        )
-	    """)
-	    List<Trainer> findActiveTrainersNotAssignedToTrainee(@Param("traineeUsername") String traineeUsername);
+
+	List<Trainer> findByUserIsActive(boolean isActive);
 }
