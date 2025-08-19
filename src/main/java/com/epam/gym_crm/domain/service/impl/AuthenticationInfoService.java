38ed9250a9e8ab002e71f24c64ae46fb4536ef97
Berat <berat230@gmail.com>
2025-08-19 05:11:01 +0300

    feat:Implement AuthenticationInfoService
    added AuthenticationInfoService for other service to get currentUserName

---
+++ src/main/java/com/epam/gym_crm/domain/service/impl/AuthenticationInfoService.java
@@ -0,0 +1,21 @@
+package com.epam.gym_crm.domain.service.impl;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import com.epam.gym_crm.domain.exception.BaseException;
+import com.epam.gym_crm.domain.exception.ErrorMessage;
+import com.epam.gym_crm.domain.exception.MessageType;
+
+@Service
+public class AuthenticationInfoService {
+
+	public String getCurrentUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+        	throw new BaseException(new ErrorMessage(MessageType.UNAUTHORIZED, "User is not authenticated."));
+        }
+        return authentication.getName();
+    }
+}
