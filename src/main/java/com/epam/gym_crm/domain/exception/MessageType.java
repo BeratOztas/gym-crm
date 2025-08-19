e13fca2909363c4ad544f0a0fa31986dba897e2b
Berat <berat230@gmail.com>
2025-08-19 05:10:12 +0300

    update MessageType.
    Added new MessageType
    TOKEN_IS_EXPIRED("1014", "Token is expired."),
    USER_NOT_FOUND("1013","User Not Found."),

--- src/main/java/com/epam/gym_crm/domain/exception/MessageType.java
+++ src/main/java/com/epam/gym_crm/domain/exception/MessageType.java
@@ -4,7 +4,9 @@ public enum MessageType {
 
 	GENERAL_EXCEPTION("9999", "An unexpected error occurred."),
 	RESOURCE_NOT_FOUND("1001", "Resource not found."),
+	TOKEN_IS_EXPIRED("1014", "Token is expired."),
 	UNAUTHORIZED("401","User authentication required"),
+	USER_NOT_FOUND("1013","User Not Found."),
 	ENTITY_NOT_FOUND("1010","Entity Not Found."),
 	PATH_BODY_MISMATCH("1012", "URL path variable does not match the request body identifier."),
 	FORBIDDEN("403","You are not authorized."),
