4335d2b93084b515d48241371301861fd7d40f16
Berat <berat230@gmail.com>
2025-08-19 05:34:16 +0300

    refactor(metrics): Refine login metrics with user-specific tags
    Updated the AppMetrics class to provide more granular metrics for login attempts.
    
    - Modified `incrementLoginSuccess` and `incrementLoginFailure` methods to accept a `username` parameter.
    - Replaced the generic `Counter` with a dynamically created one that includes the username as a tag.
    - This change allows for per-user metric tracking, providing deeper insights into login behavior and security-related events.

--- src/main/java/com/epam/gym_crm/monitoring/metric/AppMetrics.java
+++ src/main/java/com/epam/gym_crm/monitoring/metric/AppMetrics.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
 
 @Component
 public class AppMetrics {
@@ -11,17 +12,14 @@ public class AppMetrics {
 	private final Counter traineeCreationCounter;
 	private final Counter trainerCreationCounter;
 	private final Counter trainingCreationCounter;
-	private final Counter loginSuccessCounter;
-	private final Counter loginFailureCounter;
+	private final MeterRegistry meterRegistry;
 
 	public AppMetrics(MeterRegistry meterRegistry) {
-		
+		this.meterRegistry = meterRegistry;
+
 		this.traineeCreationCounter = meterRegistry.counter("gym_crm_creations_total", "type", "trainee");
 		this.trainerCreationCounter = meterRegistry.counter("gym_crm_creations_total", "type", "trainer");
 		this.trainingCreationCounter = meterRegistry.counter("gym_crm_creations_total", "type", "training");
-
-		this.loginSuccessCounter = meterRegistry.counter("gym_crm_login_attempts_total", "status", "success");
-		this.loginFailureCounter = meterRegistry.counter("gym_crm_login_attempts_total", "status", "failure");
 
 	}
 
@@ -39,12 +37,14 @@ public class AppMetrics {
 		trainingCreationCounter.increment();
 	}
 
-	public void incrementLoginSuccess() {
-		loginSuccessCounter.increment();
+	public void incrementLoginSuccess(String username) {
+		meterRegistry.counter("gym_crm_login_attempts_total", Tags.of("status", "success", "username", username))
+				.increment();
 	}
 
-	public void incrementLoginFailure() {
-		loginFailureCounter.increment();
+	public void incrementLoginFailure(String username) {
+		meterRegistry.counter("gym_crm_login_attempts_total", Tags.of("status", "failure", "username", username))
+				.increment();
 	}
 
 }
