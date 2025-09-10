4332af6688217d35e419361eafe91cdc621d6e53
Berat <berat230@gmail.com>
2025-09-10 21:39:36 +0300

    -implement  FeignClients
    Added EnableFeignClients anatation

--- src/main/java/com/epam/gym_crm/GymCrmApplication.java
+++ src/main/java/com/epam/gym_crm/GymCrmApplication.java
@@ -2,8 +2,10 @@ package com.epam.gym_crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class GymCrmApplication {
 
 	public static void main(String[] args) {
